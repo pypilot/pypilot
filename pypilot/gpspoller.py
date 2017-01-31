650423de2ab0ac382ab431529a66ca567ed9a34d
Sean D'Epagnier <seandepagnier@gmail.com>
2017-01-31 13:47:19 +0000

    move gps probe logic to separate service

--- pypilot/gpspoller.py
+++ pypilot/gpspoller.py
@@ -7,7 +7,7 @@
 # License as published by the Free Software Foundation; either
 # version 3 of the License, or (at your option) any later version.  
 
-import gps, multiprocessing, time
+import gps, multiprocessing, time, socket
 
 from signalk.values import *
 
@@ -15,62 +15,34 @@ class GpsProcess(multiprocessing.Process):
     def __init__(self):
         self.queue = multiprocessing.Queue()
         super(GpsProcess, self).__init__(target=self.gps_process, args=(self.queue, ))
-        self.show_gps_fail = True
 
-    def probe(self):
-        print 'GPS probe'
-        import os
-        if not os.system('timeout -s KILL -t 5 gpsctl'):
-            return True
+    def connect(self):
+        while True: # connection to gpsd loop
+            try:
+                self.gpsd = gps.gps(mode=gps.WATCH_ENABLE) #starting the stream of info
+                self.gpsd.next() # flush initial message
+                print 'GPS connected to gpsd'
+                return
+            except socket.error:
+                time.sleep(3)
 
-        # try to probe all possible usb devices
-        devices = ['/dev/gps', '/dev/ttyUSB', '/dev/ttyAMA', '/dev/ttyS']
-        for device in devices:
-            for i in range(4):
-                name = device + '%d' % i
-                if not os.path.exists(name):
-                    continue
-                print 'GPS probing:', name
-                if not os.system('timeout -s KILL -t 5 gpsctl -f ' + name):
-                    os.environ['GPSD_SOCKET'] = '/tmp/gpsd.sock'
-                    os.system('gpsdctl add ' + name)
-                    print 'GPS found: ' + name
-                    return True
-        return False
+    def read(self, queue):
+        while True:
+            try:
+                self.gpsd.next()
+                queue.put(self.gpsd.fix)
+            except StopIteration:
+                print 'GPS lost gpsd'
+                break
 
     def gps_process(self, queue):
-        while True:
-            try:
-                gpsd = gps.gps(mode=gps.WATCH_ENABLE) #starting the stream of info
-                gpsd.activated = False
-                break
-            except:
-                if self.show_gps_fail:
-                    self.show_gps_fail = False
-                    print 'GPS Process failed to connect to gpsd.'
+        try:
+            while True:
+                self.connect()
+                self.read(queue)
+        except KeyboardInterrupt:
+            print 'Keyboard interrupt, gps process exit'
             
-                time.sleep(3)
-            
-        print 'GPS Process connected to gpsd.'
-        self.show_gps_fail = True
-            
-        while True:
-            while not gpsd.activated:
-                if self.probe():
-                    gpsd.activated = True
-                else:
-                    time.sleep(5)
-
-            try:
-                gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
-                queue.put(gpsd.fix)
-            except KeyboardInterrupt:
-                print 'Keyboard interrupt, gps process exit'
-                break
-            except:
-                print 'GPS Process lost gpsd'
-                break
-
             
 class GpsPoller():
     def __init__(self, server):
@@ -78,6 +50,7 @@ class GpsPoller():
         self.track = self.Register(SensorValue, 'track', self)
         self.speed = self.Register(SensorValue, 'speed', self)
         self.fix = False
+        self.lastfix = False
         self.process = False
         self.timestamp = time.time()
 
@@ -89,21 +62,27 @@ class GpsPoller():
             self.process = GpsProcess()
             self.process.start()
         
-        if self.process.queue.qsize() == 0:
-            return
+        if self.process.queue.qsize() > 0:
+            # flush queue entries
+            while self.process.queue.qsize() > 1:
+                self.process.queue.get()
 
-        # flush queue entries
-        while self.process.queue.qsize() > 1:
-            self.process.queue.get()
+            # last entree
+            self.fix = self.process.queue.get()
+            if self.fix and self.fix.mode == 3:
+                self.track.set(self.fix.track)
+                self.speed.set(self.fix.speed)
+                #self.timestamp = self.fix.time
+                self.timestamp = time.time()
+            else:
+                self.fix = False
 
-        self.fix = self.process.queue.get()
-        if self.fix and self.fix.mode == 3:
-            self.track.set(self.fix.track)
-            self.speed.set(self.fix.speed)
-            #self.timestamp = self.fix.time
-            self.timestamp = time.time()
+        if time.time() - self.timestamp > 3:
+            self.fix = False
 
-            return self.fix
+        if (not self.fix) != (not self.lastfix):
+            print 'GPS ' + ('got' if self.fix else 'lost') + ' fix'
+            self.lastfix = self.fix
 
 if __name__ == "__main__":
     from signalk.server import SignalKServer
@@ -111,6 +90,7 @@ if __name__ == "__main__":
     gpsp = GpsPoller(server)
     count = 0;
     while True:
-        print gpsp.poll(), count
+        gpsp.poll()
+        print 'GPSPoller demo', count, gpsp.fix
         count += 1
         time.sleep(1)
---
+++ scripts/gpsdate.py
@@ -0,0 +1,36 @@
+#!/usr/local/bin/python
+#
+#   Copyright (C) 2017 Sean D'Epagnier
+#
+# This Program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.  
+
+# automatically set system clock to gps time if available
+
+import os, sys, time
+import gps
+
+while True:
+    while True:
+        try:
+            gpsd = gps.gps(mode=gps.WATCH_ENABLE) #starting the stream of info
+            break
+        except:
+            time.sleep(3)
+
+    while True:
+        try:
+            gpsd.next()
+        except KeyboardInterrupt:
+            exit(1)
+        except:
+            break
+
+        if len(gpsd.utc):
+            date, t = gpsd.utc[:-5].split('T')
+            print 'Setting date to gps time', date, t
+            sys.stdout.flush()
+            os.system('date -u -s "' + date + ' ' + t + '"')
+            time.sleep(3*24*60*60) # sync again in 3 days
