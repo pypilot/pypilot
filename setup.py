633e2d9c9bb6d50c456043dc177962f64ca526c9
Sean D'Epagnier <seandepagnier@gmail.com>
2017-03-19 10:12:42 -0400

    correct new paths

--- setup.py
+++ setup.py
@@ -14,9 +14,9 @@ except ImportError:
 import os, os.path
 
 locale_files = []
-for walk in os.walk('pypilot_lcd/locale'):
+for walk in os.walk('lcd/locale'):
     path, dirs, files = walk
-    path = path[len('pypilot_lcd/'):]
+    path = path[len('lcd/'):]
     for file in files:
         if file[len(file)-3:] == '.mo':
             locale_files.append(os.path.join(path, file))
@@ -27,16 +27,16 @@ setup (name = 'pypilot',
        license = 'GPLv3',
        author="Sean D'Epagnier",
        url='http://pypilot.org/',
-       packages=['pypilot', 'pypilot_ui', 'pypilot_lcd', 'pypilot_webapp', 'signalk'],
-       package_data={'pypilot_lcd': ['font.ttf'] + locale_files,
-                     'pypilot_webapp': ['static/*', 'templates/*']},
+       packages=['pypilot', 'ui', 'lcd', 'webapp', 'signalk'],
+       package_data={'lcd': ['font.ttf'] + locale_files,
+                     'webapp': ['static/*', 'templates/*']},
 #       requires=['flask', 'gevent'], # webapp
        entry_points={
            'console_scripts': [
                'pypilot=pypilot.simple_autopilot:main',
-               'pypilot_webapp=pypilot_webapp.pypilot_webapp:main',
-               'pypilot_lcdclient=pypilot_lcd.client:main',
-               'pypilot_control=pypilot_ui.autopilot_control:main',
+               'pypilot_webapp=webapp.pypilot_webapp:main',
+               'pypilot_lcdclient=lcd.client:main',
+               'pypilot_control=ui.autopilot_control:main',
                'signalk_client=signalk.client:main',
                'signalk_scope=signalk.scope:main',
                'signalk_client_wx=signalk.client_wx:main',
