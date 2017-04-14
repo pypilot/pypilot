295eb894432fd504c60d6b7db0264b8680695c8d
Sean D'Epagnier <seandepagnier@gmail.com>
2017-04-14 13:33:27 -0400

    add weather sensor sketches

--- arduino/weathersensors/weathersensors.ino
+++ arduino/weathersensors/weathersensors.ino
@@ -14,6 +14,8 @@
 #include <HardwareSerial.h>
 #include <Wire.h>
 
+const int analogInPin = A7;  // Analog input pin that the potentiometer is attached to
+
 uint8_t have_bmp280 = 0;
 uint16_t dig_T1, dig_P1;
 int16_t dig_T2, dig_T3, dig_P2, dig_P3, dig_P4, dig_P5, dig_P6, dig_P7, dig_P8, dig_P9;
@@ -34,10 +36,7 @@ void bmp280_setup()
   Wire.endTransmission();
   
   Wire.requestFrom(0x76, 1);
-  uint8_t id = Wire.read();
-  Serial.println(id);
-      
-  if(id != 0x58)
+  if(Wire.read() != 0x58)
       return;
 
   have_bmp280 = 1;
@@ -83,10 +82,26 @@ void bmp280_setup()
   Wire.endTransmission();
 }
 
+volatile unsigned int rotation_count;
+volatile unsigned long lastt;
+volatile unsigned long lastperiod;
+void isr_anenometer_count()
+{
+  unsigned long t  = millis();
+  if(t - lastt > 15){
+    rotation_count++;
+    lastperiod = t - lastt;
+    lastt = t;
+  }
+}
+
 void setup()
 {
   Serial.begin(38400);  // start serial for output
   bmp280_setup();
+
+  attachInterrupt(0, isr_anenometer_count, FALLING);
+  pinMode(2, INPUT_PULLUP);
 }
 
 int32_t t_fine;
@@ -138,65 +153,108 @@ void send_nmea(const char *buf)
   Serial.print(buf2);
 }
 
+void read_anenometer() {
+  static float lpdir, lp = .05;
+  
+  // read the analog in value:
+  int  sensorValue = analogRead(analogInPin);            
+
+  float dir = sensorValue / 1024.0 * 360;
+  lpdir = lp*dir + (1-lp)*lpdir;
+
+  // print the results to the serial monitor:
+  static float knots;
+  static int nowind;
+  static unsigned long last_time;
+  unsigned long time = millis();
+  unsigned long dt = time - last_time;
+  
+  if(dt >= 250) { // output every 250ms
+      last_time = time;
+    if(rotation_count > 0) {
+        rotation_count = 0;
+        nowind = 0;
+    } else
+        nowind++;
+
+    if(nowind == 20)
+      knots = 0, nowind--;
+    else
+      knots = .868976 * 2.25 * 1000 / lastperiod;
+
+    char buf[128];
+    snprintf(buf, sizeof buf, "APMWV,%d.%d,R,%d.%d,N,A", (int)lpdir, (int)(lpdir*10)%10, (int)knots, (int)(knots*10)%10);
+    send_nmea(buf);
+  }
+}
+
+
 uint32_t pressure, temperature;
-int count;
+int bmp280_count;
+void read_pressure_temperature()
+{
+    Wire.beginTransmission(0x76);
+    Wire.write(0xF7);
+    Wire.endTransmission();
+  
+    Wire.requestFrom(0x76, 6);    // request 6 bytes from slave device #2
+
+    uint8_t buf[6] = {0};
+    int i = 0;
+    while(Wire.available())    // slave may send less than requested
+    {
+        buf[i++] = Wire.read();
+        if(i == 6)
+            break;
+    }     
+    
+    int32_t p, t;
+    
+    p = (int32_t)buf[0] << 16 | (int32_t)buf[1] << 8 | (int32_t)buf[2];
+    t = (int32_t)buf[3] << 16 | (int32_t)buf[4] << 8 | (int32_t)buf[5];
+    
+    if(t == 0 || p == 0) {
+        Serial.println("reset bmp280");
+        bmp280_setup();
+        //  delay_ms(100);
+        return;
+    }
+
+    pressure += p >> 2;
+    temperature += t >> 2;
+    bmp280_count++;
+   
+    if(bmp280_count == 1024) {
+        bmp280_count = 0;
+        pressure >>= 12;
+        temperature >>= 12;
+    
+        int32_t temperature_comp = bmp280_compensate_T_int32(temperature);
+        int32_t pressure_comp = bmp280_compensate_P_int64(pressure);
+  
+        char buf[128];
+        int a, b, c, r;
+        a = pressure_comp/25600;
+        r = pressure_comp%25600;
+        b = r / 2560;
+        c = (r - b*2560) / 256;
+        snprintf(buf, sizeof buf, "PYMDA,%d.%d%d,B,,,,,,,,,,,,,,,,,,", a, b, c);
+        send_nmea(buf);
+
+        a = temperature_comp / 100;
+        r = temperature_comp % 100;
+        b = r/10;
+        c = r-b*10;
+        snprintf(buf, sizeof buf, "PYMTA,%d.%d%d,C", a, b, c);
+        send_nmea(buf);
+
+        pressure = temperature = 0;
+    }
+}
+
 void loop()
 {
-  Wire.beginTransmission(0x76);
-  Wire.write(0xF7);
-  Wire.endTransmission();
-  
-  Wire.requestFrom(0x76, 6);    // request 6 bytes from slave device #2
-
-  uint8_t buf[6] = {0};
-  int i = 0;
-  while(Wire.available())    // slave may send less than requested
-  {
-      buf[i++] = Wire.read();
-      if(i == 6)
-         break;
-  }     
-      
-  int32_t p, t;
- 
-  p = (int32_t)buf[0] << 16 | (int32_t)buf[1] << 8 | (int32_t)buf[2];
-  t = (int32_t)buf[3] << 16 | (int32_t)buf[4] << 8 | (int32_t)buf[5];
-  
-  if(t == 0 || p == 0) {
-    Serial.println("reset bmp280");
-    bmp280_setup();
-  //  delay_ms(100);
-    return;
-  }
-
-  pressure += p;
-  temperature += t;
-  count++;
-
-  if(count == 256) {
-    pressure >>= 12;
-    temperature >>= 12;
-    
-    int32_t temperature_comp = bmp280_compensate_T_int32(temperature);
-    int32_t pressure_comp = bmp280_compensate_P_int64(pressure);
-  
-    char buf[128];
-    int a, b, c, r;
-    a = pressure_comp/25600;
-    r = pressure_comp%25600;
-    b = r / 2560;
-    c = (r - b*2560) / 256;
-    snprintf(buf, sizeof buf, "PYMDA,%d.%d%d,B,,,,,,,,,,,,,,,,,,", a, b, c);
-    send_nmea(buf);
-
-    a = temperature_comp / 100;
-    r = temperature_comp % 100;
-    b = r/10;
-    c = r-b*10;
-    snprintf(buf, sizeof buf, "PYMTA,%d.%d%d,C", a, b, c);
-    send_nmea(buf);
-
-    pressure = temperature = 0;
-    count = 0;
-  }
+    if(have_bmp280)
+        read_pressure_temperature();
+    read_anenometer();
 }
