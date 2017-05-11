0725b35d5119d7c03ae7e9d0689f6af5f3621137
Sean D'Epagnier <seandepagnier@gmail.com>
2017-05-11 21:40:00 +0000

    improve default cal

--- pypilot/servo.py
+++ pypilot/servo.py
@@ -455,7 +455,7 @@ class Servo(object):
             self.calibration.set(json.loads(file.readline()))
         except:
             print 'WARNING: using default servo calibration!!'
-            self.calibration.set({'forward': [0, .6], 'reverse': [0, .6]})
+            self.calibration.set({'forward': [.2, .6], 'reverse': [.2, .6]})
 
     def save_calibration(self):
         file = open(Servo.calibration_filename, 'w')
