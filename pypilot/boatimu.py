ae7a9ef586f5241ce66662a9f13c7d00d4148510
Sean D'Epagnier <seandepagnier@gmail.com>
2018-09-11 21:53:23 -0400

    set period to 10hz until further testing

--- pypilot/boatimu.py
+++ pypilot/boatimu.py
@@ -240,8 +240,8 @@ def heading_filter(lp, a, b):
     return result
 
 class BoatIMU(object):
-  #period = .1 # 10hz
-  period = .04; # 25hz
+  period = .1 # 10hz
+  #period = .04; # 25hz
   def __init__(self, server, *args, **keywords):
     self.server = server
 
