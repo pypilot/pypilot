3ae7b848365bcd259fcf09d34cc86ce966aed49b
Sean D'Epagnier <seandepagnier@gmail.com>
2019-06-11 23:51:38 +0000

    add version to boot splash

--- lcd/ugfx/splash.cpp
+++ lcd/ugfx/splash.cpp
@@ -14,22 +14,31 @@
 
 #include "ugfx.h"
 
+
+#define LOAD() \
+    const char *data = header_data; \
+    unsigned char pixel[4] = {0}; \
+    char data32[4*width*height], *p = data32; \
+    for(int i = 0; i<width*height; i++) { \
+        HEADER_PIXEL(data, pixel); \
+        memcpy(p, pixel, 4); \
+        p += 4; \
+    } \
+    return new surface(width, height, bypp, data32);
+
 surface *load_logo(int bypp)
 {    
     #include "pypilot_logo.h"
-    const char *data = header_data;
-    unsigned char pixel[4] = {0};
-    unsigned char data32[4*width*height], *p = data32;
-
-    for(int i = 0; i<width*height; i++) {
-        HEADER_PIXEL(data, pixel);
-        memcpy(p, pixel, 4);
-        p += 4;
-    }
-    
-    return new surface(width, height, bypp, data32);
+    LOAD()
+    #undef HEADER_PIXEL
 }
 
+surface *load_version(int bypp)
+{    
+    #include "pypilot_version.h"
+    LOAD()
+    #undef HEADER_PIXEL
+}
 
 int main(int argc, char *argv[])
 {
@@ -41,6 +50,7 @@ int main(int argc, char *argv[])
 
 #if 1
     surface *logo = load_logo(framebuffer->bypp);
+    surface *version = load_version(framebuffer->bypp);
 #else
     const char *path = "/home/tc/.pypilot/splash.surf";
     surface *logo = new surface(path);
@@ -61,6 +71,9 @@ int main(int argc, char *argv[])
     surface *logom = new surface(framebuffer);
 
     int fac = facf;
+
+    logo->blit(version, logo->width - version->width, logo->height - version->height);
+
     logom->magnify(logo, fac);
     logom->invert(0, 0, logom->width, logom->height);
 
