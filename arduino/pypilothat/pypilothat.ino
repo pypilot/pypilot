a212e81645bbbdeb9a1aba24e776d9a824eb83a5
Sean D'Epagnier <seandepagnier@gmail.com>
2019-12-02 23:44:27 -0500

    remove serial debugging

--- arduino/pypilothat/pypilothat.ino
+++ arduino/pypilothat/pypilothat.ino
@@ -7,7 +7,7 @@
 #include <avr/boot.h>
 #include <util/delay.h>
 
-
+//#define DEBUG_SERIAL
 
 #include <RCSwitch.h>
 
@@ -42,7 +42,9 @@ void setup() {
 
     pinMode(MISO, OUTPUT);
 
+#ifdef DEBUG_SERIAL
     Serial.begin(38400);
+#endif
 
   pinMode(DATA_PIN, INPUT);
   pinMode(DIR_PIN, INPUT);
@@ -59,34 +61,29 @@ void loop() {
     int value = mySwitch.getReceivedValue();
     
     if (value == 0) {
+#ifdef DEBUG_SERIAL
       Serial.print("Unknown encoding");
+#endif
     } else {
-   
-        Serial.print("Received ");
+#ifdef DEBUG_SERIAL
+      Serial.print("Received ");
       Serial.print( mySwitch.getReceivedValue() );
       Serial.print(" / ");
       Serial.print( mySwitch.getReceivedBitlength() );
       Serial.print("bit ");
       Serial.print("Protocol: ");
       Serial.println( mySwitch.getReceivedProtocol() );
-
-
+      unsigned int *raw = mySwitch.getReceivedRawdata();
+      Serial.print("timings: ");
+      for(int i=0; i<16; i++) {
+          Serial.print(raw[i]);
+                  Serial.print(" ");
+      }
+      Serial.println("");
+#endif
       if(mySwitch.getReceivedBitlength() == 24)
           spiread_rfkeys = mySwitch.getReceivedValue();
-      
-#if 0
-              unsigned int *raw = mySwitch.getReceivedRawdata();
-        Serial.print("timings: ");
-        for(int i=0; i<16; i++) {
-            Serial.print(raw[i]);
-                    Serial.print(" ");
-        }
-        Serial.println("");
-#endif
     }
-    
-    mySwitch.resetAvailable();
-    
+    mySwitch.resetAvailable(); 
   }
-
 }
