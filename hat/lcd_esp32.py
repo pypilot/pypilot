c1e3c6f633d11e41ed361c074cc4a99c57a134eb
Sean D'Epagnier <seandepagnier@gmail.com>
2020-07-31 20:58:30 -0400

    improve power and sleep use for remote

--- hat/lcd_esp32.py
+++ hat/lcd_esp32.py
@@ -43,6 +43,7 @@ gpio_esp32.init(lcd)
 t5= time.time()
 print ('loaded', t5-t0, ':',t1-t0, t2-t1, t3-t2, t4-t3, t5-t4)
 
+sleepmode = 0
 while True:
     import gc
     gc.collect()
@@ -53,23 +54,47 @@ while True:
         lcd.battery_voltage = v
     lp = .02
     lcd.battery_voltage = (1-lp)*lcd.battery_voltage + lp*v
-    #print('batt', lcd.battery_voltage)
 
     gpio_esp32.poll(lcd)
-
     if any(list(map(lambda key : key.count, lcd.keypad))):
         sleeptime = time.time()
-        tft.backlight(True)
+        if sleepmode:
+            tft.backlight(True)
+        if sleepmode > 1:
+            machine.freq(240000000)
+        sleepmode = 0
     
     t0 = time.time()
-    lcd.poll()
+    try:
+        lcd.poll()
+    except Exception as e:
+        print('lcd poll failed', e)
     t1 = time.time()
     gpio_esp32.poll(lcd)
     t2 = time.time()
         
+    if time.time() - sleeptime > 20:
+        #print('sleep blank screen')
+        tft.backlight(False)
+        #esp.sleep_type(esp.SLEEP_MODEM) # SLEEP_LIGHT
+        sleepmode = 1
+
+    if time.time() - sleeptime > 60:
+        if wifi_esp32.station.isconnected():
+            #print('sleep wifi off')
+            #wifi_esp32.station.active(False)
+            pass
+        #machine.freq(80000000)
+        sleepmode = 2
+
+    #if wifi_esp32.station.isconnected():
     wifi_esp32.poll(lcd.client)
+
+    if time.time() - sleeptime > 300:
+        print('sleep power down')
+        machine.deepsleep()
+
     t3 = time.time()
-    
     dt = t3-t0
     s = period - dt
     if s <= .01:
@@ -77,9 +102,3 @@ while True:
         #print('sleep ', t1-t0, t2-t1, t3-t2, s*100/(t3-t0+s), '%')
 
     time.sleep(s)
-
-    if time.time() - sleeptime > 600:
-        #tft.backlight(False)
-        #esp.sleep_type(esp.SLEEP_MODEM) # SLEEP_LIGHT
-        #time.sleep(.1)
-        pass
