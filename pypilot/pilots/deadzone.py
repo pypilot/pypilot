2c61b78015b98c9d0cc1010648eab62718d188be
Sean D'Epagnier <seandepagnier@gmail.com>
2023-11-10 09:43:07 -0400

    add experimental pilots

---
+++ pypilot/pilots/deadzone.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+#
+#   Copyright (C) 2023 Sean D'Epagnier
+#
+# This Program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.  
+
+
+# this pilot is an experiment to compare performance to others
+
+from pilot import AutopilotPilot
+from resolv import resolv
+from pypilot.values import *
+
+class DeadZonePilot(AutopilotPilot):
+  def __init__(self, ap, name='deadzone'):
+    super(DeadZonePilot, self).__init__(name, ap)
+    self.deadzone = self.register(RangeProperty, 'deadzone', 5, 1, 30, persistent=True, profiled=True)
+
+    self.PosGain('P',  .003, .15)   # position (heading error)
+    self.PosGain('D',  .09, .3)    # derivative (gyro)
+    self.PosGain('DD', .075, 0.3) # rate of derivative
+    self.PosGain('FF', .6, 3.0)   # feed forward
+
+  def process(self):
+    t = time.monotonic()
+    ap = self.ap
+    
+    # compute command
+    headingrate = ap.boatimu.SensorValues['headingrate_lowpass'].value
+    headingraterate = ap.boatimu.SensorValues['headingraterate_lowpass'].value
+
+    if abs(ap.heading_error.value) < self.deadzone.value:
+        command = 0
+    else:
+        gain_values = {'P': ap.heading_error.value,
+                       'D': headingrate,      
+                       'DD': headingraterate,
+                       'FF': ap.heading_command_rate.value}
+        command = self.Compute(gain_values)
+        if abs(command) < ap.servo.speed.min.value:
+            command = 0
+      
+    if ap.enabled.value: # perform absolute servo command (not delayed by period)
+        ap.servo.command.set(command)
+
+pilot = DeadZonePilot
---
+++ pypilot/pilots/rate.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+#
+#   Copyright (C) 2023 Sean D'Epagnier
+#
+# This Program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.  
+
+from pilot import AutopilotPilot
+from resolv import resolv
+from pypilot.values import *
+
+class RatePilot(AutopilotPilot):
+  def __init__(self, ap, name='rate'):
+    super(RatePilot, self).__init__(name, ap)
+
+    # create extended pid filter
+    self.PosGain('D', .075, 0.3) # rate of derivative
+    self.PosGain('DD', .075, 0.3) # rate of derivative
+    self.PosGain('FF', .6, 3.0)   # feed forward
+
+    self.maxturnrate = self.register(RangeProperty, 'maxturnrate', 2, .5, 30, persistent=True)
+    self.turnraterate = self.register(RangeProperty, 'turnraterate', .5, .1, 5, persistent=True, profiled=True)
+
+
+  def process(self):
+    t = time.monotonic()
+    ap = self.ap
+    
+    # compute command
+    headingerror = ap.heading_error.value
+    headingrate = ap.boatimu.SensorValues['headingrate_lowpass'].value
+    headingraterate = ap.boatimu.SensorValues['headingraterate_lowpass'].value
+
+    maxturnrate = self.maxturnrate.value
+    raterate = self.turnraterate.value
+
+    rate = min(max(headingerror*raterate, -maxturnrate), maxturnrate)
+
+    rateerror = headingrate - rate
+    gain_values = {'D': rateerror,
+                   'DD': headingraterate
+                   'FF': ap.heading_command_rate.value}
+
+    command = self.Compute(gain_values)
+    if ap.enabled.value:
+        ap.servo.command.command(command)
+
+pilot = RatePilot
