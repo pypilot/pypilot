de415829492898b70e851aef736018f76d3644aa
Dominik Röttsches <d@rtsh.es>
2025-05-17 20:23:16 +0300

    Add Evdev Remote control based on evdev input events
    Select and grab an input device to listen for keyboard events
    and send those to pypilot, see documentation in
    pypilot/evdev_remote/remote.py

---
+++ pypilot/evdev_remote/log_config.py
@@ -0,0 +1,13 @@
+import logging
+
+
+def setup_logging():
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    return logger
+
+
+logger = setup_logging()
+
+__all__ = ["logger"]
