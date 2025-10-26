da9c62352c7bf406bd533b5ee4ac313180e17608
Simon Letort <367992+simonletort@users.noreply.github.com>
2025-10-26 21:31:13 +0000

    Fix NameError in dependencies.py
    - Changed undefined variable 'i' to caught exception 'e' in sys_dep.test() method
    - Fixes installation crash when system dependency check fails
    - Exception was being caught but wrong variable name was used in error handling

--- web/web.py
+++ web/web.py
@@ -7,7 +7,12 @@
 # version 3 of the License, or (at your option) any later version.  
 
 import sys, os
-from flask import Flask, render_template, session, request, Markup
+from flask import Flask, render_template, session, request
+
+try:
+    from flask import Markup
+except ImportError:
+    from markupsafe import Markup
 
 from flask_socketio import SocketIO, Namespace, emit, join_room, leave_room, \
     close_room, rooms, disconnect
